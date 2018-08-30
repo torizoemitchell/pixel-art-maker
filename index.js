529a0081d945b7d6d3181124f0245540596b217d
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-08-30 16:16:17 -0600

    create canvas and brushes

---
+++ index.js
@@ -0,0 +1,56 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+//---------------------------Create Canvas Start-------------------
+//find body
+let body = document.getElementsByTagName('body')[0]
+// make a table element to create the canvas
+let canvas = document.createElement('table')
+//add the table element to the body
+body.appendChild(canvas)
+//loop to create rows in the table
+const rowNum = 10
+for (let j = 0; j < rowNum; j++) {
+  let row = document.createElement('tr')
+  const colNum = 20
+  for(let i = 0; i < colNum; i++){
+    let col = document.createElement('td')
+    col.classList.add('square')
+    row.appendChild(col)
+  }
+  canvas.appendChild(row)
+}
+
+
+//-----------------------------Create Canvas End------------------------
+
+
+// // Add an event listener to each div so when clicked the background turns red.
+let turnRed = function(event){
+  let square = event.target
+  square.classList.toggle('red')
+}
+canvas.addEventListener('click', turnRed)
+// Add an event listener to div tags so when clicked the brush color is saved.
+//document.getElementsByClassName('brush')[0].style[0]
+
+// let saveCurrentColor = function(event){
+//   //store the element that was clicked in brush
+//   let brush = event.target
+//   //save the
+//   let brushColor = brush[0].style[0]
+//   let currentColor =
+// }
+
+// let paintBrushGroup = document.getElementsByClassName('paintbrush-group')[0]
+// paintBrushGroup.addEventListener('click', saveCurrentColor)
+
+// Expand the dimensions of the pixel canvas.
+// Expand the palette with more colors. (i.e. red, orange, yellow, green, blue, purple, brown, gray, black, white, etc.)
+// Expand the palette with a brush color indicator.
+// Improve the look and feel of the canvas and color palette. Be careful not to break your program's behavior!
+
+
+
+
+
+})
