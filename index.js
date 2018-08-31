7a8951e2c3850811d7be0904e3996694134ed790
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-08-31 11:46:08 -0600

    mostly working except will not toggle color selector

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ let canvas = document.createElement('table')
 //add the table element to the body
 body.appendChild(canvas)
 //loop to create rows in the table
-const rowNum = 10
+const rowNum = 25
 for (let j = 0; j < rowNum; j++) {
   let row = document.createElement('tr')
-  const colNum = 20
+  const colNum = 55
   for(let i = 0; i < colNum; i++){
     let col = document.createElement('td')
     col.classList.add('square')
@@ -19,10 +19,6 @@ for (let j = 0; j < rowNum; j++) {
   }
   canvas.appendChild(row)
 }
-
-
-//-----------------------------Create Canvas End------------------------
-
 
 // // Add an event listener to each div so when clicked the background turns red.
 // let turnRed = function(event){
@@ -34,23 +30,31 @@ for (let j = 0; j < rowNum; j++) {
 //document.getElementsByClassName('brush')[0].style[0]
 
 let saveCurrentColor = function(event){
-  //store the element that was clicked in brush
+  //---------------------------get the paintbrush color---------------------------------------
+  //store the paintbrush element that was clicked in brush
   console.log("event.target: ", event.target)
   let brush = event.target
-  //save the
+  //brush.classList[1] stores the class with the background color of the paintbrush element
   let brushColor = brush.classList[1]
+  //rename to "currentColor" for clarity
   let currentColor = brushColor
   console.log("currentColor: ", currentColor)
+
+  //----------------display the paintbrush color in the display box----------------------------
+  //store the current color display box element in a variable
   let currentColorDisplayBox = document.getElementById('currentColorDisplayBox')
-  //check to see if current color is in currentColorDisplayBox.classList
-  if(currentColorDisplayBox.classList.value === 'current-color'){
-    currentColorDisplayBox.classList.remove('current-color')
+  //check to see if default-color is in currentColorDisplayBox.classList. This means that no paintbrush has been selected yet.
+  if(currentColorDisplayBox.classList.value === 'default-color'){
+    //replace the current default-color with the class storing the currentColor background-color
+    currentColorDisplayBox.classList.remove('default-color')
     currentColorDisplayBox.classList.add(currentColor)
   }else{
     currentColorDisplayBox.classList.remove(currentColorDisplayBox.classList.value)
     currentColorDisplayBox.classList.add(currentColor)
   }
-  console.log('classList: ', currentColorDisplayBox.classList.value)
+  console.log('classList: ', currentColorDisplayBox.classList)
+
+  //-----------------turn the pixel the color of the current color display box-------------------
   //change pixel color
   let turnColor = function(event){
     let square = event.target
@@ -64,10 +68,6 @@ paintBrushGroup.addEventListener('click', saveCurrentColor)
 
 
 
-
-// Expand the dimensions of the pixel canvas.
-// Expand the palette with more colors. (i.e. red, orange, yellow, green, blue, purple, brown, gray, black, white, etc.)
-// Expand the palette with a brush color indicator.
 // Improve the look and feel of the canvas and color palette. Be careful not to break your program's behavior!
 
 
