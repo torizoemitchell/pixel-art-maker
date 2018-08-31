26caa37106cdf63cb862302810496ab2e6b8a7a7
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-08-31 12:37:57 -0600

    finished! could use style to make it pretty.

--- index.js
+++ index.js
@@ -26,19 +26,19 @@ for (let j = 0; j < rowNum; j++) {
 //   square.classList.toggle('red')
 // }
 // canvas.addEventListener('click', turnRed)
-// Add an event listener to div tags so when clicked the brush color is saved.
-//document.getElementsByClassName('brush')[0].style[0]
+
+
+// Add an event listener to div tags so when clicked the brush color is saved
 
 let saveCurrentColor = function(event){
   //---------------------------get the paintbrush color---------------------------------------
   //store the paintbrush element that was clicked in brush
-  console.log("event.target: ", event.target)
+  //console.log("event.target: ", event.target)
   let brush = event.target
-  //brush.classList[1] stores the class with the background color of the paintbrush element
+  //brush.classList[1] stores a string which is the class with the background color of the paintbrush element
   let brushColor = brush.classList[1]
   //rename to "currentColor" for clarity
   let currentColor = brushColor
-  console.log("currentColor: ", currentColor)
 
   //----------------display the paintbrush color in the display box----------------------------
   //store the current color display box element in a variable
@@ -46,19 +46,49 @@ let saveCurrentColor = function(event){
   //check to see if default-color is in currentColorDisplayBox.classList. This means that no paintbrush has been selected yet.
   if(currentColorDisplayBox.classList.value === 'default-color'){
     //replace the current default-color with the class storing the currentColor background-color
+    //console.log('before removing default class: ', currentColorDisplayBox.classList)
     currentColorDisplayBox.classList.remove('default-color')
+    //console.log('after removing default class: ', currentColorDisplayBox.classList)
     currentColorDisplayBox.classList.add(currentColor)
+    //console.log('after adding new color: ', currentColorDisplayBox.classList)
   }else{
+    //console.log('before removing paintbrush color: ', currentColorDisplayBox.classList)
     currentColorDisplayBox.classList.remove(currentColorDisplayBox.classList.value)
+    //console.log('after removing color: ', currentColorDisplayBox.classList)
     currentColorDisplayBox.classList.add(currentColor)
+    //console.log('after adding new color: ', currentColorDisplayBox.classList)
   }
-  console.log('classList: ', currentColorDisplayBox.classList)
+
 
   //-----------------turn the pixel the color of the current color display box-------------------
   //change pixel color
+
   let turnColor = function(event){
+    //store the pixel that was clicked in a variable
     let square = event.target
-    square.classList.toggle(currentColor)
+    //change the color of the square to the color that is stored in currnt color display box.
+    // console.log('square classList before adding new color: ', square.classList)
+    // console.log('square.classList.value before: ', square.classList.value)
+    //create a string that will always be "square" to keep the square css properties. You'll append the scurrent color to this.
+    let squareClass = square.classList.value.substring(0,6)
+    //create a string with the name of the class you want to get rid of
+    let oldColor = square.classList.value.substring(7)
+
+    //check to see if the classList value string is less than 6 (this means) no paintbrush class has been added before
+    if(square.classList.value.length < 7){
+      //just add the current color
+      //console.log("adding: ", currentColor)
+      square.classList.add(currentColor)
+    }else{
+      //remove the old color and add the new color
+      //console.log("removing: ", oldColor)
+      square.classList.remove(oldColor)
+      //console.log("adding: ", currentColor)
+      square.classList.add(currentColor)
+    }
+
+    //console.log('square classList after adding new color: ', square.classList)
+    //console.log('squareclassList.value after: ', square.classList.value)
   }
   canvas.addEventListener('click', turnColor)
 
